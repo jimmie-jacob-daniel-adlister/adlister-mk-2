3c9dc6c1d21a41f3f6db619f6c7894bb4d8253b0
james McBride <james.m.mcbride15@gmail.com>
2021-03-18 14:04:23 -0500

    added functionality to users page

--- src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
@@ -1,11 +1,16 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.*;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "controllers.ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
@@ -14,6 +19,21 @@ public class ViewProfileServlet extends HttpServlet {
             response.sendRedirect("/login");
             return;
         }
+        User user = (User) request.getSession().getAttribute("user");
+        List<Ad> ads = DaoFactory.getUsersDao().allUserAds(user.getId());
+        for(Ad ad : ads){
+            long postId=ad.getId();
+            ArrayList<PostCategories> categories = DaoFactory.getPostsCategoriesDao().findByPostId(postId);
+            ad.setCategories(categories);
+            System.out.println(ad.getCategories());
+            ArrayList<Image> images = DaoFactory.getImagesDao().findByPostId(postId);
+            ad.setImages(images);
+            System.out.println(ad.getImages());
+            int commentsId= (int) postId;
+            List<Comment> comments=DaoFactory.getCommentsDao().all(commentsId);
+            ad.setComments(ad.getComments());
+        };
+        request.setAttribute("ads", ads);
         request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }
 }
--- src/main/webapp/WEB-INF/profile.jsp
+++ src/main/webapp/WEB-INF/profile.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -11,6 +12,31 @@
     <div class="container">
         <h1>Welcome, ${sessionScope.user.username}!</h1>
     </div>
+    <c:forEach var="ad" items="${ads}">
+        <div class="col-md-6">
+            <h2>${ad.title}</h2>
+            <p>${ad.description}</p>
+            <c:forEach var="category" items="${ad.categories}">
+                <div class="col-md-6">
+                    <p>${category.getCategory()}</p>
+                </div>
+            </c:forEach>
+            <div>Comments</div>
+            <c:forEach var="comment" items="${ad.comments}">
+                <div class="col-md-6">
+                    <h2>${comment.userId}</h2>
+                    <p>${comment.content}</p>
+                </div>
+            </c:forEach>
+            <c:forEach var="image" items="${ad.images}">
+                <div class="col-md-6">
+                    <h2>${image.description}</h2>
+                    <img src="${image.url}"></img>
+                </div>
+            </c:forEach>
+
+        </div>
+    </c:forEach>
 
 </body>
 </html>
